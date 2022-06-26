9a55fb01aa2043130b464dd88c9a22d829985cbc
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-06-26 23:07:40 +0200

    Create an error-handling middleware function

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((error, request, response, next) => {
+  console.error(error.stack);
+  response.status(500).send('Something broke!');
+});
+
+
 app.use (morgan('common'));
