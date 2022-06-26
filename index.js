c3ada7f9b75524180cd17a7e742527218b4b7b09
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-06-26 23:08:13 +0200

    add top 10 movies

--- index.js
+++ index.js
@@ -17,4 +17,48 @@ app.use((error, request, response, next) => {
 });
 
 
+let topMovies = 
+[
+    {
+    name : 'Avatar',
+    year : 2009
+    },
+    {
+    name : 'Avengers:Endgame',
+    year : 2019
+    },
+    {
+    name : 'Star Wars',
+    year : 2015
+    },
+    {
+    name : 'Avengers: Infinity War',
+    year : 2018
+    },
+    {
+    name : 'Spider-Man: No Way Home',
+    year : 2021
+    },
+    {
+    name : 'Jurassic World',
+    year : 2015
+    },
+    {
+    name : 'The Lion King',
+    year : 2019
+    },
+    {
+    name : 'Frozen II',
+    year : 2019
+    },
+    {
+    name : 'Harry Potter and the Deathly Hallows: Part 2',
+    year : 2011
+    },
+    {
+    name : 'Beauty and the Beast',
+    year : 2017
+    }
+];
+
 app.use (morgan('common'));
