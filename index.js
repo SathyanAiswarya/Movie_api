956540a732d0dfbeba161c207e24c24e6555ffb6
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-06-28 20:43:37 +0200

    amend the error handling middleware and format.

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser'),
+const express = require('express'),
+  app = express(),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
   methodOverride = require('method-override');
+
+
+let topMovies =
+  [
+    {
+      name: 'Avatar',
+      year: 2009
+    },
+    {
+      name: 'Avengers:Endgame',
+      year: 2019
+    },
+    {
+      name: 'Star Wars',
+      year: 2015
+    },
+    {
+      name: 'Avengers: Infinity War',
+      year: 2018
+    },
+    {
+      name: 'Spider-Man: No Way Home',
+      year: 2021
+    },
+    {
+      name: 'Jurassic World',
+      year: 2015
+    },
+    {
+      name: 'The Lion King',
+      year: 2019
+    },
+    {
+      name: 'Frozen II',
+      year: 2019
+    },
+    {
+      name: 'Harry Potter and the Deathly Hallows: Part 2',
+      year: 2011
+    },
+    {
+      name: 'Beauty and the Beast',
+      year: 2017
+    }
+  ];
+
+app.use(morgan('common'));
+
+app.get('/', (request, response) => {
+  response.send('Welcome to MyFlix');
+});
+app.get('/topmovies', (request, response) => {
+  response.json(topMovies);
+});
+
+app.use('/', express.static('public'));
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,63 +72,7 @@ app.use((error, request, response, next) => {
   response.status(500).send('Something broke!');
 });
 
-
-let topMovies = 
-[
-    {
-    name : 'Avatar',
-    year : 2009
-    },
-    {
-    name : 'Avengers:Endgame',
-    year : 2019
-    },
-    {
-    name : 'Star Wars',
-    year : 2015
-    },
-    {
-    name : 'Avengers: Infinity War',
-    year : 2018
-    },
-    {
-    name : 'Spider-Man: No Way Home',
-    year : 2021
-    },
-    {
-    name : 'Jurassic World',
-    year : 2015
-    },
-    {
-    name : 'The Lion King',
-    year : 2019
-    },
-    {
-    name : 'Frozen II',
-    year : 2019
-    },
-    {
-    name : 'Harry Potter and the Deathly Hallows: Part 2',
-    year : 2011
-    },
-    {
-    name : 'Beauty and the Beast',
-    year : 2017
-    }
-];
-
-app.use (morgan('common'));
-
-app.get('/',(request,response)=>{
-     response.send('Welcome to MyFlix');
- });
-app.get('/topmovies',(request,response)=>{
-    response.json(topMovies);
-});
-
-app.use('/', express.static('public'));
-
 app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+  console.log('Your app is listening on port 8080.');
+});
 
